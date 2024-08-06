125644b8db6aff58886c2d247b2fc4b657dbe67e
cslesani <cs.lesani@gmail.com>
2024-08-06 15:24:52 +0330

    Add vendor changes to add_vendor branch

--- adminpanellogin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminpanelloginConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adminpanellogin'
--- adminpanellogin/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import admin_login
-from .views import admin_logout
-
-urlpatterns = [
-    path('', admin_login, name='admin_login'),
-    path('admin-logout/', admin_logout, name='admin_logout'),
-]
